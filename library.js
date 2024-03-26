d72443ec7ed459a1dfb92c12eb1af3756a403283
manelly67 <manellylopezqqq@gmail.com>
2024-03-26 08:52:12 -0500

    initial files

---
+++ library.js
@@ -0,0 +1,24 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+
+
+
+/* function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return( this.title );
+    return(this.author);
+  };
+}
+   */
